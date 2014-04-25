50342f7084970db39c1868c5e7419f99bcdb488b
Robin Swenson-Healey <rjswenson@gmail.com>
2014-04-24 17:20:56 -0700

    passing

--- spec/features/builder/sizing_screen_spec.rb
+++ spec/features/builder/sizing_screen_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "The sizing screen" do
+describe "The sizing screen", :sauce => true do
   it "can be visited from order page", :js => true do
     sign_in_rep
     create_populated_order
@@ -8,9 +8,8 @@ describe "The sizing screen" do
       find("span.dijitReset.dijitButtonText", :text => "Sizing").click
     end
     page.should have_content("Quick Add")
-    page.should have_content("Units:0")
+    page.should have_content("Units: 0")
     destroy_order
-    sign_out
   end
 
   it "has items added to cart previously", :js => true do
@@ -26,13 +25,12 @@ describe "The sizing screen" do
       @s3_prod_url == find("img")['src']
     end
     destroy_order
-    sign_out
   end
 
   it "add amounts updates price and # units", :js => true do
     sign_in_rep
     create_populated_order
-    dojo_visit("builder,sizing")
+    find("span.dijitReset.dijitInline.dijitButtonText", :text => "Sizing").click
     first("span.priceLabel").value == "$0.00"
     within("div.row.stockItem", :match => :first) do
       find("input.dijitReset.dijitInputInner").set("1")
@@ -41,17 +39,16 @@ describe "The sizing screen" do
     find("p.units").value == "Units:1"
     first("span.priceLabel").value != "$0.00"
     destroy_order
-    sign_out
   end
 
-  it "can add amounts to size variants", :js => true do
-    pending
-  end
+  # it "can add amounts to size variants", :js => true do
+  #   pending
+  # end
 
   it "can remove a product from their list", :js => true do
     sign_in_rep
     create_populated_order
-    dojo_visit("builder,sizing")
+    find("span.dijitReset.dijitInline.dijitButtonText", :text => "Sizing").click
     within("div.variationItems", :match => :first) do
       page.should have_content("Units")
       page.should have_content("$")
@@ -59,7 +56,6 @@ describe "The sizing screen" do
     end
     page.should_not have_css("div.variationItems")
     destroy_order
-    sign_out
   end
 end
 
